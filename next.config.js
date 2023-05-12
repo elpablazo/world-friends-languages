52d62fc79d2549ad00dac6d26c34b81d084a237b
Obed Vargas <obeskay.mail@gmail.com>
2023-05-11 23:42:38 -0600

    css custom

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = withPayload(
     configPath: path.resolve(__dirname, "./payload/payload.config.ts"),
 
     // Point to custom Payload CSS (optional)
-    // cssPath: path.resolve(__dirname, "./css/my-custom-payload-styles.css"),
+    cssPath: path.resolve(__dirname, "./src/styles/payloadOverride.css"),
 
     // Point to your exported, initialized Payload instance (optional, default shown below`)
     payloadPath: path.resolve(process.cwd(), "./payload/payloadClient.ts"),
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontWeight: {
+      default: 400,
+      bold: 700,
+    },
+    fontFamily: {
+      sans: "Ubuntu, sans-serif",
+    },
     extend: {
+      colors: {
+        primary: {
+          light: "#00A0AA",
+          DEFAULT: "#01A6A5",
+          dark: "#00AAA0",
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
