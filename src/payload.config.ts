3757fadb79f2a27b38520832bdab5c7e1cffdff0
Pablo <52553480+elpablazo@users.noreply.github.com>
2023-05-11 22:35:18 -0600

    tags y posts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,13 +3,15 @@ import path from "path";
 // import Examples from './collections/Examples';
 import Users from "./collections/Users";
 import Clases from "./collections/Clases";
+import Posts from "./collections/Posts";
+import Tags from "./collections/Tags";
 
 export default buildConfig({
   serverURL: "http://localhost:3000",
   admin: {
     user: Users.slug,
   },
-  collections: [Users, Clases],
+  collections: [Users, Clases, Posts, Tags],
   typescript: {
     outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
