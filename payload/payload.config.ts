29c3ac7a57b2f97c4d60a6787e05f7ebdc15901a
Obed Vargas <obeskay.mail@gmail.com>
2023-05-12 00:08:09 -0600

    Logo cambiado

--- payload/payload.config.ts
+++ payload/payload.config.ts
@@ -4,8 +4,8 @@ import Users from "./collections/Users";
 import Clases from "./collections/Clases";
 import Posts from "./collections/Posts";
 import Tags from "./collections/Tags";
-import Logo from "./graphics/Logo";
-import Icon from "./graphics/Icon";
+import Logo from "./components/graphics/Logo";
+import Icon from "./components/graphics/Icon";
 
 export default buildConfig({
   collections: [Users, Clases, Posts, Tags],
