8792f7de905546636b7092e76ace28c2905a4ed9
Pablo <52553480+elpablazo@users.noreply.github.com>
2023-05-11 23:39:46 -0600

    colecciones pirujas

--- payload/payload.config.ts
+++ payload/payload.config.ts
@@ -1,14 +1,16 @@
-import path from 'path';
-import { buildConfig } from 'payload/config';
+import path from "path";
+import { buildConfig } from "payload/config";
+import Users from "./collections/Users";
+import Clases from "./collections/Clases";
+import Posts from "./collections/Posts";
+import Tags from "./collections/Tags";
 
 export default buildConfig({
-  collections: [
-    // Your collections here
-  ],
+  collections: [Users, Clases, Posts, Tags],
   globals: [
     // Your globals here
   ],
   typescript: {
-    outputFile: path.resolve(__dirname, '../payload-types.ts'),
+    outputFile: path.resolve(__dirname, "../payload-types.ts"),
   },
 });
