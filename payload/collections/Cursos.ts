381245b2cd6ba726aab7a2d67a815b7625650915
Pablo <52553480+elpablazo@users.noreply.github.com>
2023-05-19 22:27:08 -0600

    CORRECCIONES AL HORARIO

--- payload/collections/Cursos.ts
+++ payload/collections/Cursos.ts
@@ -168,12 +168,16 @@ const Cursos: CollectionConfig = {
     },
   ],
   hooks: {
+    beforeChange: [
+      async ({ req, operation, originalDoc }) => {
+        console.log("originalDoc: ", originalDoc);
+      },
+    ],
     // En este hook, creamos las clases automáticamente cuando se crea un curso según los horarios establecidos
     afterChange: [
       async ({ doc, req, operation }) => {
-        console.log("doc-----------------", doc);
-
         if (operation === "create") {
+          console.log(doc);
           // Creamos una clase para cada horario en el periodo especificado
           const startDate = new Date(doc.startDate);
           const endDate = new Date(doc.endDate);
@@ -189,21 +193,26 @@ const Cursos: CollectionConfig = {
           // Guardamos el número de clases que se van a crear
           let numberOfClassesCreated = 0;
 
-          // Creamos una clase para cada día de la semana
-          for (let i = 0; i < days.length; i++) {
-            const day = days[i];
+          // Obtenemos los distintos horarios del curso
+          const schedules = doc.schedule;
+
+          // Para cada horario, creamos las clases
+          for (let i = 0; i < schedules.length; i++) {
+            const schedule = schedules[i];
+
+            // tODO: HACER CONCORDAR EL NÚMERO DE CLASE (PORQUE PRIMERO SE CREAN TODAS LAS CLASES DE UN HORARIO EJ: LUNES, Y LUEGO LAS DE MARTES, ETC. Y ENTONCES LAS DE MARTES NO TIENEN EL NÚMERO CORRECTO)
 
             // Creamos una clase para cada día de la semana
             for (
-              let date = startDate;
-              date <= endDate;
+              let date = new Date(startDate);
+              date <= new Date(endDate);
               date.setDate(date.getDate() + 1)
             ) {
               // Si el día de la semana de la fecha actual es igual al día de la semana del horario, creamos la clase
-              if (date.getDay() === i) {
+              if (date.getDay() === days.indexOf(schedule.day)) {
                 numberOfClassesCreated++;
-                const startTime = new Date(doc.schedule[0].startTime);
-                const endTime = new Date(doc.schedule[0].endTime);
+                const startTime = new Date(schedule.startTime);
+                const endTime = new Date(schedule.endTime);
 
                 // Creamos la clase
                 const newClass = {
@@ -219,10 +228,15 @@ const Cursos: CollectionConfig = {
                   collection: "clases",
                   data: newClass,
                 });
+
+                // Imprimimos la fecha de la clase y el título
+                console.log("Clase creada: ", date, " -", clase.name);
               }
             }
           }
         }
+
+        console.log("Terminé.");
 
         return doc;
       },
