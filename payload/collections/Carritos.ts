bedeb91a2ca466a8f6a189a45516ab34517850d9
Pablo <52553480+elpablazo@users.noreply.github.com>
2023-05-20 13:37:18 -0600

    Le puse tabs al carrito

--- payload/collections/Carritos.ts
+++ payload/collections/Carritos.ts
@@ -5,26 +5,46 @@ const Carritos: CollectionConfig = {
   fields: [
     // Relación con los cursos
     {
-      type: "relationship",
-      name: "cursos",
-      label: "Cursos",
-      relationTo: "cursos",
-      hasMany: true,
-    },
-    {
-      type: "relationship",
-      name: "users",
-      relationTo: "users",
-      // Todo: sólo un user de rol "alumno" puede tener un carrito
-      hasMany: false,
-      admin: {
-        position: "sidebar",
-      },
-    },
-    {
-      type: "number",
-      name: "total",
-      label: "Total",
+      type: "tabs",
+      tabs: [
+        // Tab de información general
+        {
+          label: "Información general",
+          description: "Información general del carrito.",
+          fields: [
+            {
+              type: "relationship",
+              name: "cursos",
+              label: "Cursos",
+              relationTo: "cursos",
+              hasMany: true,
+            },
+            {
+              type: "relationship",
+              name: "users",
+              relationTo: "users",
+              label: "Usuario que tiene el carrito",
+              // Todo: sólo un user de rol "alumno" puede tener un carrito
+              // Todo: solo un admin puede cambiar a quién le pertenece el carrito
+              hasMany: false,
+              admin: {
+                position: "sidebar",
+              },
+            },
+            {
+              type: "number",
+              name: "total",
+              label: "Total",
+            },
+            {
+              type: "checkbox",
+              name: "pagado",
+              label: "Pagado",
+              defaultValue: false,
+            },
+          ],
+        },
+      ],
     },
   ],
   hooks: {
@@ -36,9 +56,17 @@ const Carritos: CollectionConfig = {
         originalDoc, // original document
       }) => {
         // Sumamo el total de los cursos
-        const total = data.cursos.reduce((acc: any, curso: any) => {
-          return acc + curso.price;
-        });
+        let total = 0;
+        for (const cursoid of data.cursos) {
+          const curso = await req.payload.findByID({
+            collection: "cursos",
+            id: cursoid,
+          });
+
+          console.log(curso);
+
+          total += Number(curso.price);
+        }
         data.total = total;
         return data;
       },
