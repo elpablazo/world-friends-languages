9600363c814fa6d43f7ea32631f39ee0b5d05456
Obed Vargas <obeskay.mail@gmail.com>
2023-06-15 20:28:32 -0600

    Avance: /cursos
    Se agrega el banner principal a la página de cursos, por el momento solo es la versión de escritorio.

--- app/cursos/page.tsx
+++ app/cursos/page.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+const bannerSrc = "/images/demo/bannerCursos.jpg";
+
 const Page = (): JSX.Element => {
-  return <div className="container py-12"></div>;
+  return (
+    <div className="relative min-h-screen space-y-6">
+      <Image
+        src={bannerSrc}
+        alt="Banner de los cursos | World Friend Languages"
+        priority
+        width={3840}
+        height={762}
+        className="aspect-[3840/762] w-full h-auto my-12"
+      />
+    </div>
+  );
 };
+
+export default Page;
