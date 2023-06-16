54279d4c67522e157b04cc21e40c151274f71c07
Obed Vargas <obeskay.mail@gmail.com>
2023-06-15 22:59:59 -0600

    Mejora: Las transiciones entre páginas ocurren más suavemente

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,7 @@ import "./globals.css";
 import { Ubuntu } from "next/font/google";
 import Footer from "@/components/footer";
 import { Toaster } from "@/components/ui/toaster";
-import {
-  AnimatePresence,
-  MotionConfig,
-  cubicBezier,
-  motion,
-} from "framer-motion";
+import { AnimatePresence, MotionConfig, circOut, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
 const ubuntu = Ubuntu({
@@ -28,22 +23,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
+
   return (
     <MotionConfig
       transition={{
-        ease: cubicBezier(0.124, 0.25, 0.63, 1),
+        ease: circOut,
         duration: 0.75,
       }}
     >
       <html lang="es-MX" className="bg-light text-dark antialiased">
         <body className={`${ubuntu.className} overflow-x-clip max-w-screen`}>
           <Navbar />
-          <AnimatePresence mode="popLayout">
+          <AnimatePresence mode="wait">
             <motion.div
+              className="opacity-0"
               key={pathname}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
+              exit={{ opacity: 0, transition: { delay: 0 } }}
+              transition={{
+                delay: 0.2,
+              }}
             >
               {children}
             </motion.div>
