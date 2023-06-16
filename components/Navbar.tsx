45eaefbdf78ee67147f170e8e0c20a99dcf36256
Obed Vargas <obeskay.mail@gmail.com>
2023-06-15 20:29:48 -0600

    Mejora: Menú móvil en navbar y links
    Se mejora el menú móvil, ya tiene efectos y animaciones, hace falta añadirle el icono de cerrar cuando se esté abierto el menú. También hace falta añadir funcionalidad a la barra de búsqueda.
    
    También los links ya funcionan.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import { motion } from "framer-motion";
 import Logo from "./payload/Logo";
 import Icon from "./Icon";
 import Link from "next/link";
@@ -12,6 +13,17 @@ const Navbar = () => {
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const links = [
+    { href: "/cursos", label: "Ver cursos" },
+    { href: "/clases", label: "Clases 1 a 1" },
+    { href: "/marco-teorico", label: "Marco teórico" },
+  ];
+
+  const menuVariants = {
+    open: { opacity: 1, y: 0 },
+    closed: { opacity: 0, y: "-100%" },
   };
 
   return (
@@ -38,17 +50,15 @@ const Navbar = () => {
             />
           </div>
 
-          <div className="text-dark text-center whitespace-nowrap font-bold">
-            Ver cursos
-          </div>
-
-          <div className="text-dark text-center whitespace-nowrap font-bold">
-            Clases 1 a 1
-          </div>
-
-          <div className="text-dark text-center whitespace-nowrap font-bold">
-            Marco teórico
-          </div>
+          {links.map((link) => (
+            <Link
+              href={link.href}
+              key={link.href}
+              className="text-dark text-center whitespace-nowrap font-bold"
+            >
+              {link.label}
+            </Link>
+          ))}
 
           <Link href={"/registrate"} className="block">
             <Button size="sm" variant="secondary">
@@ -56,33 +66,66 @@ const Navbar = () => {
             </Button>
           </Link>
         </div>
-        <div className="lg:hidden cursor-pointer" onClick={toggleMenu}>
+        <div className="xl:hidden cursor-pointer" onClick={toggleMenu}>
           <Icon variant={"menu"} />
         </div>
       </div>
 
-      <div
+      <motion.div
         className={`${
-          isMenuOpen ? "flex lg:hidden" : "hidden"
-        } md:hidden fixed top-0 left-0 w-screen flex-col items-center justify-center h-screen bg-white`}
+          isMenuOpen ? "flex xl:hidden" : "hidden"
+        } xl:hidden fixed top-0 left-0 w-screen flex-col items-center justify-start h-screen bg-white/70 backdrop-blur-sm`}
+        variants={menuVariants}
+        initial="closed"
+        animate={isMenuOpen ? "open" : "closed"}
       >
-        <div className="flex flex-col gap-8 items-center justify-center">
-          <div className="lg:hidden cursor-pointer" onClick={toggleMenu}>
-            <Icon variant={"menu"} />
+        <div className="flex flex-col gap-8 items-center justify-center py-4 w-full">
+          <div className="container">
+            <div className="grid gap-4 grow w-full">
+              <div className="flex flex-row items-center justify-between w-full">
+                <Logo />
+                <div onClick={toggleMenu}>
+                  <Icon variant={"menu"} />
+                </div>
+              </div>
+              <div className="grid gap-1 grow w-full">
+                <Label className="sr-only" htmlFor="search">
+                  Buscar
+                </Label>
+                <Input
+                  id="search"
+                  placeholder="Buscar"
+                  type="text"
+                  autoCapitalize="none"
+                  autoComplete="off"
+                  autoCorrect="off"
+                  disabled={false}
+                  className="w-full"
+                />
+              </div>
+              <div className="grid gap-0 my-2">
+                {links.map((link, i) => (
+                  <Link
+                    href={link.href}
+                    className="text-dark text-center py-4 border-b border-gray-light w-full last:border-none text-lg"
+                    key={i}
+                  >
+                    {link.label}
+                  </Link>
+                ))}
+              </div>
+              <Link
+                href={"/registrate"}
+                className="flex mx-auto w-full max-w-sm"
+              >
+                <Button variant="secondary" className="w-full">
+                  Ingresa
+                </Button>
+              </Link>
+            </div>
           </div>
-          <div className="text-dark text-center">Busca clases o cursos</div>
-
-          <div className="text-dark text-center">Ver cursos</div>
-
-          <div className="text-dark text-center">Clases 1 a 1</div>
-
-          <div className="text-dark text-center">Marco teórico</div>
-
-          <Link href={"/registrate"}>
-            <Button variant="secondary">Ingresa</Button>
-          </Link>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
