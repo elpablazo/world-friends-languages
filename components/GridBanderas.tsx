84229b9c4acbb0ea49a1de0b9684adbe8595e602
Obed Vargas <obeskay.mail@gmail.com>
2023-06-15 22:33:40 -0600

    Mejora: Se añade typescript a GridBanderas

--- components/GridBanderas.tsx
+++ components/GridBanderas.tsx
@@ -1,23 +1,41 @@
-const GridBanderas = () => {
+"use client";
+import { motion } from "framer-motion";
+import BanderaThumbnail, { iBandera } from "./BanderaThumbnail";
+import React, { memo } from "react";
+
+type Props = {
+  banderas: iBandera[];
+};
+
+const GridBanderas = ({ banderas }: Props) => {
   return (
-    <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="bg-gray-100 rounded-lg shadow-lg p-4">
-          <div className="flex justify-center">
-            <img
-              src="/images/demo/banderas/alemania.jpg"
-              alt="Estados Unidos"
-              className="w-24 h-24"
-            />
-          </div>
-          <div className="text-center">
-            <h3 className="text-2xl font-semibold">Estados Unidos</h3>
-            <p className="text-gray-500">Inglés</p>
-          </div>
-        </div>
-      </div>
+    <div className="flex gap-4 items-center w-[400vh]">
+      {Array(4)
+        .fill(null)
+        .map((item: any, i: number) => {
+          return (
+            <motion.div
+              key={i}
+              className="flex flex-row gap-4 overflow-x-clip items-center justify-start w-[100vh] -mr-[15vh]"
+              animate={{ x: -100 * banderas.length }}
+              transition={{
+                repeat: Infinity,
+                duration: 20,
+                ease: "linear",
+              }}
+            >
+              {banderas.map((bandera, j) => (
+                <div key={j} className="select-none">
+                  <BanderaThumbnail {...bandera} />
+                </div>
+              ))}
+            </motion.div>
+          );
+        })}
     </div>
   );
 };
 
-export default GridBanderas;
+GridBanderas.displayName = "GridBanderas";
+
+export default memo(GridBanderas);
