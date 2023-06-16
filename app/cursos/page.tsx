4e218cfcb106dc86ea466fde1abed60649944f8b
Obed Vargas <obeskay.mail@gmail.com>
2023-06-15 22:34:33 -0600

    Se complementa la página de cursos

--- app/cursos/page.tsx
+++ app/cursos/page.tsx
@@ -1,4 +1,10 @@
+import GridBanderas from "@/components/GridBanderas";
 import Image from "next/image";
+import { iBandera } from "@/components/BanderaThumbnail";
+import CardCursoLP from "@/components/CardCursoLP";
+import Icon from "@/components/Icon";
+import SearchBar from "@/components/SearchBar";
+import { Button } from "@/components/ui/button";
 const bannerSrc = "/images/demo/bannerCursos.jpg";
 
 const Page = (): JSX.Element => {
@@ -12,6 +18,93 @@ const Page = (): JSX.Element => {
         height={762}
         className="aspect-[3840/762] w-full h-auto my-12"
       />
+      <div className="relative !-mt-[10vh]">
+        <div className="container mx-auto px-4 max-w-md">
+          <div className="card flex flex-col gap-4 bg-white rounded p-4">
+            <h1 className="text-2xl lg:text-3xl font-bold text-center">
+              Cursos de idiomas
+            </h1>
+            <div className="flex gap-4 flex-col lg:flex-row">
+              <SearchBar />
+              <Button className="w-full mx-auto lg:w-min">Buscar</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <GridBanderas
+        banderas={[
+          {
+            pais: "Francia",
+            imagenUrl: "/images/demo/banderas/francia.jpg",
+          },
+
+          {
+            pais: "Alemania",
+            imagenUrl: "/images/demo/banderas/alemania.jpg",
+          },
+          {
+            pais: "Reino Unido",
+            imagenUrl: "/images/demo/banderas/reino-unido.jpg",
+          },
+          {
+            pais: "Italia",
+            imagenUrl: "/images/demo/banderas/italia.jpg",
+          },
+          {
+            pais: "Estados Unidos",
+            imagenUrl: "/images/demo/banderas/usa.jpg",
+          },
+          {
+            pais: "Portugal",
+            imagenUrl: "/images/demo/banderas/portugal.jpg",
+          },
+        ]}
+      />
+      <div className="container">
+        <div className="grid gap-6">
+          {[
+            {
+              titulo: "Inglés para certificado TOEFL",
+              descripcion:
+                "Prepárate para el éxito en el examen TOEFL con nuestro curso de inglés. Aprenderás estrategias efectivas para mejorar tus habilidades en comprensión auditiva, lectura, expresión escrita y oral.",
+              imagen: "/images/demo/banderas/usa.jpg",
+            },
+            {
+              titulo: "Curso básico de Alemán",
+              descripcion:
+                "Aprende alemán desde cero con nuestro curso básico. Aprenderás a comunicarte en alemán en situaciones cotidianas y a comprender textos sencillos.",
+              imagen: "/images/demo/banderas/alemania.jpg",
+            },
+            {
+              titulo: "Francés intermedio",
+              descripcion:
+                "Prepárate para hablar Francés como un experto. Mejorarás pronunciación, gramática y vocabulario. Aprenderás a comunicarte en situaciones cotidianas y a comprender textos sencillos.",
+              imagen: "/images/demo/banderas/francia.jpg",
+            },
+          ].map((curso, i) => {
+            return (
+              <CardCursoLP
+                key={i}
+                titulo={curso.titulo}
+                imagenUrl={curso.imagen}
+              >
+                <div className="flex flex-col gap-4">
+                  <p>{curso.descripcion}</p>
+                  <div className="flex flex-row gap-2 text-primary text-base">
+                    <div className="mt-1">
+                      <Icon variant="reloj" />
+                    </div>
+                    <div className="grid">
+                      <span className="font-bold">Duración:</span>
+                      <span className="text-dark">6 meses</span>
+                    </div>
+                  </div>
+                </div>
+              </CardCursoLP>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
