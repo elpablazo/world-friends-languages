392644bfa2bde79324098f3419256ba67bb0826c
Pablo <52553480+elpablazo@users.noreply.github.com>
2023-06-16 20:37:36 -0600

    Bug: Corregí error de stroke-strokemiterlimit

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -443,7 +443,7 @@ const Icon = ({ variant, extraClassName }: IIconProps) => {
             d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z"
             className={`stroke-current`}
             strokeWidth="2"
-            stroke-miterlimit="10"
+            stroke-strokemiterlimit="10"
           />
           <path
             d="M16 9V16H23"
