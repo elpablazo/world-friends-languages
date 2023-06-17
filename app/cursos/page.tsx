dddcdc062cde0d9ff6a098f914149d396f93cf58
Pablo <52553480+elpablazo@users.noreply.github.com>
2023-06-16 21:33:19 -0600

    feat(cursos): Add dynamic data for courses and refactor flag URLs in course cards
    This commit adds code to retrieve course data using Axios and update the course cards accordingly. It also refactors the way flag URLs are rendered in course cards to use the new `banderas` array.

--- app/cursos/page.tsx
+++ app/cursos/page.tsx
@@ -5,9 +5,54 @@ import CardCursoLP from "@/components/CardCursoLP";
 import Icon from "@/components/Icon";
 import SearchBar from "@/components/SearchBar";
 import { Button } from "@/components/ui/button";
+import payloadApi from "@/lib/axios";
+import Link from "next/link";
 const bannerSrc = "/images/demo/bannerCursos.jpg";
 
-const Page = (): JSX.Element => {
+async function getStaticProps() {
+  const res = await payloadApi.get("/cursos");
+
+  if (res.status !== 200) {
+    throw new Error(res.statusText);
+  }
+
+  console.log("Cursos data --------------------->", res.data);
+
+  return res.data;
+}
+
+// Todo: pasar las banderas a un componente
+const banderas = [
+  {
+    pais: "Francia",
+    imagenUrl: "/images/demo/banderas/francia.jpg",
+  },
+
+  {
+    pais: "Alemania",
+    imagenUrl: "/images/demo/banderas/alemania.jpg",
+  },
+  {
+    pais: "Reino Unido",
+    imagenUrl: "/images/demo/banderas/reino-unido.jpg",
+  },
+  {
+    pais: "Italia",
+    imagenUrl: "/images/demo/banderas/italia.jpg",
+  },
+  {
+    pais: "Estados Unidos",
+    imagenUrl: "/images/demo/banderas/usa.jpg",
+  },
+  {
+    pais: "Portugal",
+    imagenUrl: "/images/demo/banderas/portugal.jpg",
+  },
+];
+
+const Page = async () => {
+  const cursos = await getStaticProps();
+
   return (
     <div className="relative min-h-screen space-y-12">
       <Image
@@ -31,76 +76,34 @@ const Page = (): JSX.Element => {
           </div>
         </div>
       </div>
-      <GridBanderas
-        banderas={[
-          {
-            pais: "Francia",
-            imagenUrl: "/images/demo/banderas/francia.jpg",
-          },
-
-          {
-            pais: "Alemania",
-            imagenUrl: "/images/demo/banderas/alemania.jpg",
-          },
-          {
-            pais: "Reino Unido",
-            imagenUrl: "/images/demo/banderas/reino-unido.jpg",
-          },
-          {
-            pais: "Italia",
-            imagenUrl: "/images/demo/banderas/italia.jpg",
-          },
-          {
-            pais: "Estados Unidos",
-            imagenUrl: "/images/demo/banderas/usa.jpg",
-          },
-          {
-            pais: "Portugal",
-            imagenUrl: "/images/demo/banderas/portugal.jpg",
-          },
-        ]}
-      />
+      <GridBanderas banderas={banderas} />
       <div className="container">
         <div className="grid sm:grid-cols-2 gap-6">
-          {[
-            {
-              titulo: "Inglés para certificado TOEFL",
-              descripcion:
-                "Prepárate para el éxito en el examen TOEFL con nuestro curso de inglés. Aprenderás estrategias efectivas para mejorar tus habilidades en comprensión auditiva, lectura, expresión escrita y oral.",
-              imagen: "/images/demo/banderas/usa.jpg",
-            },
-            {
-              titulo: "Curso básico de Alemán",
-              descripcion:
-                "Aprende alemán desde cero con nuestro curso básico. Aprenderás a comunicarte en alemán en situaciones cotidianas y a comprender textos sencillos.",
-              imagen: "/images/demo/banderas/alemania.jpg",
-            },
-            {
-              titulo: "Francés intermedio",
-              descripcion:
-                "Prepárate para hablar Francés como un experto. Mejorarás pronunciación, gramática y vocabulario. Aprenderás a comunicarte en situaciones cotidianas y a comprender textos sencillos.",
-              imagen: "/images/demo/banderas/francia.jpg",
-            },
-          ].map((curso, i) => {
+          {cursos?.docs?.map((curso: any) => {
             return (
-              <CardCursoLP
-                key={i}
-                titulo={curso.titulo}
-                imagenUrl={curso.imagen}
-              >
-                <div className="flex flex-col gap-4">
-                  <p>{curso.descripcion}</p>
-                  <div className="flex flex-row gap-2 text-primary text-base">
-                    <div className="mt-1">
-                      <Icon variant="reloj" />
-                    </div>
-                    <div className="grid">
-                      <span className="font-bold">Duración:</span>
-                      <span className="text-dark">6 meses</span>
+              <Link href={`/cursos/${curso.id}`} key={curso.id}>
+                <CardCursoLP
+                  titulo={curso.name}
+                  imagenUrl={
+                    banderas.find(
+                      (bandera: iBandera) => bandera.pais === curso.flag
+                    )?.imagenUrl || "/images/demo/banderas/usa.jpg"
+                  }
+                >
+                  <div className="flex flex-col gap-4">
+                    <p>{curso.descripcion}</p>
+                    <div className="flex flex-row gap-2 text-primary text-base">
+                      <div className="mt-1">
+                        <Icon variant="reloj" />
+                      </div>
+                      <div className="grid">
+                        <span className="font-bold">Duración:</span>
+                        <span className="text-dark">6 meses</span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </CardCursoLP>
+                </CardCursoLP>
+              </Link>
             );
           })}
         </div>
