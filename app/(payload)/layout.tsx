17f8f6a458a1c11e6f828405ff688ceee82853d5
Obed Vargas <obeskay.mail@gmail.com>
2023-06-28 21:12:32 -0600

    Mejora: Se le retira lo innecesario a /app/payload/layout

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -1,15 +1,9 @@
 "use client";
-
-import { AnimatePresence, motion } from "framer-motion";
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const routeKey =
-    typeof window !== "undefined" ? window.location.pathname : "";
-
   return (
     <html lang="es-MX">
       <body className="relative font-sans">{children}</body>
