dc73d3f6b598467e37a2e281da1faf53bc3d9dc5
Obed Vargas <obeskay.mail@gmail.com>
2023-06-28 21:08:35 -0600

    Mejora: Se quita lo innecesario del componente CustomNav

--- components/payload/CustomNav.tsx
+++ components/payload/CustomNav.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 import MenuContainer from "../MenuContainer";
 import { motion } from "framer-motion";
 import Icon from "../Icon";
+import { Menu } from "../Menu";
 const menuItems = [
   {
     label: "Inicio",
@@ -52,15 +53,7 @@ const menuItems = [
 ];
 
 const CustomNav = () => {
-  const { permissions, user } = useAuth();
-  const [menuActive, setMenuActive] = useState(false);
-  const [groups, setGroups] = useState([]);
-
-  const { i18n } = useTranslation("general");
-
   const {
-    collections,
-    globals,
     routes: { admin },
     admin: {
       components: { beforeNavLinks, afterNavLinks },
@@ -84,76 +77,6 @@ const CustomNav = () => {
         </div>
       </div>
     </div>
-  );
-};
-
-function Menu({ items, selected }: any) {
-  return (
-    <div className="flex">
-      <ul className="block relative w-full">
-        {items.map(({ href, label, icon }: any) => (
-          <>
-            <AdminMenuItem
-              key={href}
-              href={href}
-              icon={icon}
-              selected={selected === href}
-            >
-              {label}
-            </AdminMenuItem>
-            <hr className="border-dark/5 hidden lg:block" />
-          </>
-        ))}
-        <li
-          className={`flex flex-col lg:flex-row gap-4 w-full items-center px-2 lg:px-6 cursor-pointer mt-auto py-6`}
-          onClick={() => {
-            alert("Cerrando sesión");
-          }}
-        >
-          <Icon variant="salida" />
-          <span className=" text-center lg:text-left font-bold text-lg">
-            Salir
-          </span>
-        </li>
-      </ul>
-    </div>
-  );
-}
-
-const AdminMenuItem = ({ href, selected, children, icon }: any) => {
-  return (
-    <a
-      href={href}
-      className="!block relative w-full text-center lg:text-left py-6"
-    >
-      {selected && <MenuSelectedIndicator />}
-      <motion.li
-        className={`flex flex-col lg:flex-row gap-4 !z-[1] w-full px-2 lg:px-6 cursor-pointer items-center ${
-          selected && "!text-secondary"
-        }`}
-      >
-        {icon && <Icon variant={icon} />}
-        <p className="order-last font-bold text-lg ">{children}</p>
-      </motion.li>
-    </a>
-  );
-};
-
-const MenuSelectedIndicator = () => {
-  return (
-    <motion.div
-      animate={{
-        transition: {
-          ease: "easeOut",
-          spring: 0,
-          bounce: 0,
-        },
-      }}
-      layoutId="menuIndicator"
-      className="absolute w-full h-full pointer-events-none inset-0"
-    >
-      <div className="w-full h-full bg-gradient-to-l from-secondary/100 to-secondary/0 opacity-10"></div>
-    </motion.div>
   );
 };
 
