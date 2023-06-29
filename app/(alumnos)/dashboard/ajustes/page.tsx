dc7f2b376da6cf2fb1452e2ddfc62119b2b403c1
Obed Vargas <obeskay.mail@gmail.com>
2023-06-28 21:11:08 -0600

    Nuevo: Página /dashboard/ajustes

---
+++ app/(alumnos)/dashboard/ajustes/page.tsx
@@ -0,0 +1,11 @@
+import MenuContainer from "@/components/MenuContainer";
+
+const page = () => {
+  return (
+    <MenuContainer>
+      <h1>Ajustes</h1>
+    </MenuContainer>
+  );
+};
+
+export default page;
