b467f1b14c4ab7a6b2a79886414730b9a284a1d3
Obed Vargas <obeskay.mail@gmail.com>
2023-07-04 10:10:08 -0600

    Mejora: Dashboard alumnos ya saluda

--- app/(alumnos)/dashboard/page.tsx
+++ app/(alumnos)/dashboard/page.tsx
@@ -1,9 +1,20 @@
 import MenuContainer from "@/components/MenuContainer";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 
 const page = () => {
+  const nombre_user = "Pablo";
   return (
     <MenuContainer>
-      <h1>Tus próximas clases</h1>
+      <div className="flex flex-col gap-4 w-full px-4 text-left">
+        <h1 className="font-bold text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
+          Hola, {nombre_user}.
+        </h1>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+          </CardHeader>
+        </Card>
+      </div>
     </MenuContainer>
   );
 };
